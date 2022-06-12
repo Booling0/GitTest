8bbc04230b40f10dfc68e3ca8a556e64ae8f98da
Booling0 <94294091+Booling0@users.noreply.github.com>
2022-06-12 11:05:07 +0800

    rr
    pulll

--- src/main/java/kk/test.java
+++ src/main/java/kk/test.java
@@ -4,5 +4,6 @@ public class test {
     public static void main(String[] args) {
         System.out.println("这才是我想要的");
         System.out.println("这才是我真正想要的");
+        System.out.println("这才是我真正想要的.....");
     }
 }
